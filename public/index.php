6a6a0a0d805a8c86013c5e98ac981d80fda24e48
Ferran Marlet <ferran.marlet@gmail.com>
2015-01-06 02:25:59 +0100

    Test slim

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@ echo '<br>realpath = '.realpath('../app/lib/Slim/Slim.php');
  *
  * If you are using Composer, you can skip this step.
  */
-require '../app/lib/Slim/Slim.php';
+require 'app/lib/Slim/Slim.php';
 
 \Slim\Slim::registerAutoloader();
 
