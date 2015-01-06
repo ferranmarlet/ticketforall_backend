9c4c6875111103f0bed9bd6cfd0bcae018535861
Ferran Marlet <ferran.marlet@gmail.com>
2015-01-06 03:56:55 +0100

    connection parameters

--- app/data/connection.php
+++ app/data/connection.php
@@ -1,6 +1,6 @@
 <?php
-require '../app/data/rb.php';
-require '../app/data/config.php';
+require 'rb.php';
+require 'config.php';
 
 // The following parameters are defined at /app/data/config.php
 R::setup('mysql:host='.dbHost.';dbname='.dbname,dbUser,dbPassword);
