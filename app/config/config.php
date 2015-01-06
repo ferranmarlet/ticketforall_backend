4c304846faea85b5e5357d56f09ec73d0f7d69cc
Ferran Marlet <ferran.marlet@gmail.com>
2015-01-06 04:25:34 +0100

    Heroku changes

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,11 @@
 <?php
-
-define('dbUser','testApiDBUser');
+define('dbUser','b7d231cb5ac4ea');
+define('dbPassword','106bce72');
+define('dbname','heroku_f415b945f8638bd');
+define('dbHost','localhost');
+/*define('dbUser','testApiDBUser');
 define('dbPassword','apitestpasswoorld');
 define('dbname','testApiDB');
-define('dbHost','localhost');
+define('dbHost','localhost');*/
 
 ?>
