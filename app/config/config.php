eae6d58429b614f214faa9d1423d3f2810b82b94
Ferran Marlet <ferran.marlet@gmail.com>
2015-01-08 01:28:51 +0100

    Changes specific to heroku

--- app/config/config.php
+++ app/config/config.php
@@ -2,10 +2,10 @@
 define('dbUser','b7d231cb5ac4ea');
 define('dbPassword','106bce72');
 define('dbname','heroku_f415b945f8638bd');
-define('dbHost','localhost');
+define('dbHost','us-cdbr-iron-east-01.cleardb.net');
+
 /*define('dbUser','testApiDBUser');
 define('dbPassword','apitestpasswoorld');
 define('dbname','testApiDB');
 define('dbHost','localhost');*/
-
 ?>
